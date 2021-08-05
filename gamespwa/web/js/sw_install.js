0861b04bf789241ed646efd61ceba95aef903e81
leobiriba <leobiriba10@gmail.com>
2021-08-05 20:41:04 -0300

    Fazendo alguns ajustes

--- gamespwa/web/js/sw_install.js
+++ gamespwa/web/js/sw_install.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
-    console.log("Instalando nosso serviceWorker...");
-    navigator.serviceWorker.register('/sw.js')
+    console.log("Instalando nosso serviceWorker...")
+    navigator.serviceWorker.register('/js/sw.js')
         .then(e => {
             console.log("sw.js instalado")
         })
