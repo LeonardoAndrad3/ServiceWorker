44bcabd305e62a98fdba21a35b4e2d59247e9f73
leobiriba <leobiriba10@gmail.com>
2021-08-05 20:11:11 -0300

    Fazendo upload para o heroku e o git. Nessa tarefa construimos uma troca de imagem pelo cache.

---
+++ gamespwa/web/js/sw_install.js
@@ -0,0 +1,10 @@
+if('serviceWorker' in navigator){
+    console.log("Instalando nosso serviceWorker...");
+    navigator.serviceWorker.register("sw.js")
+        .then(e => {
+            console.log("sw.js instalado")
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    }
