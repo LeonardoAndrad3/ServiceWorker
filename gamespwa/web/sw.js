acf8bc7692707b3e30439a6d9a55d1cd82851435
leobiriba <leobiriba10@gmail.com>
2021-08-05 21:05:29 -0300

    Fazendo alguns ajustes

---
+++ gamespwa/web/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('static-v1')
+            .then(cache => cache.add('./icons/images2.jpeg'))
+    );
+});
+
+self.addEventListener('activate', e => {});
+
+self.addEventListener('fetch', fethEvent => {
+    console.log('Fazendo fetch...')
+    console.log(fethEvent.request.url)
+    const rep =  new URL(fethEvent.request.url)
+    if(rep.pathname == "./icons/images1.jpeg"){
+        fetchEvent.respondWith(caches.match('./icons/images2.jpeg'))
+    }
+});
