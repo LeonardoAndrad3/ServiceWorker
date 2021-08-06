ddcfd3f6937ba0e42c48054d311360f0211b5dd9
leobiriba <leobiriba10@gmail.com>
2021-08-05 21:22:42 -0300

    Fazendo alguns ajustes

--- gamespwa/web/sw.js
+++ gamespwa/web/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch', fethEvent => {
     console.log(fethEvent.request.url)
     const rep =  new URL(fethEvent.request.url)
     if(rep.pathname == "/icons/images1.jpeg"){
-        fetchEvent.respondWith(caches.match('./icons/images2.jpeg'))
+        fethEvent.respondWith(caches.match('./icons/images2.jpeg'))
         console.log("Image2")
     }
 });
